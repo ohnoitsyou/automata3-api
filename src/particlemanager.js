52bab947f552c2c7fefdd73b3cdda9fae8940959
David Young <ohnoitsyou7043@gmail.com>
2016-05-16 00:55:48 -0400

    Updating the ParticleManager plugin
    I *think* it's complete, going to rewrite the light plugin to test

--- src/particlemanager.js
+++ src/particlemanager.js
@@ -1,6 +1,7 @@
 var Plugin = require('automata-plugin')
 var Particle = require('particle-api-js')
 var debug = require('debug')("Plugin:ParticleManager")
+var trace = require('debug')('Plugin:ParticleManager:trace')
 var EventEmitter = require('events')
 var util = require('util')
 
@@ -20,28 +21,31 @@ class ParticleManager extends Plugin {
   }
 
   login(username, password) {
-    return new Promise((resolve, reject) => {
-      if(accessToken == null) {
-        particle.login({username: username, password: password}).then((data) => {
-          accessToken = data.body.access_token
-          debug("Login success, Access Token: %s", accessToken)
-          setTimeout((this.getDevices).bind(this),1000)
-          refreshInterval = setInterval((this.getDevices).bind(this),deviceRefreshInterval)
-          this.emit('particle-login')
-          resolve(true)
-        }, (err) => {
-          reject("Login failure, check username and password")
-        })
-      } else {
-        resolve('Already logged in, success')
-      }
-    })
+    if(accessToken == null) {
+      return particle.login({username: username, password: password}).then((resp) => {
+        accessToken = resp.body.access_token
+        debug('Login success, Access token: %s', accessToken)
+        this.getDevices()
+        refreshInterval = setInterval((this.getDevices).bind(this),deviceRefreshInterval)
+        trace('Emitting: login-success')
+        this.emit('login-success')
+        return Promise.resolve('Login success')
+      }).catch((error) => {
+        debug('login failure: %s', error)
+        trace('Emitting: login-failure')
+        this.emit('login-failure')
+        return Promise.rejct(new Error('Login failure, Check username and password'))
+      })
+    } else {
+      return Promise.resolve('Already logged in, success')
+    }
   }
 
   getDevices() {
     if(accessToken != null) {
-      particle.listDevices({auth: accessToken}).then((data) => {
-        for(var device of data.body) {
+      return particle.listDevices({auth: accessToken}).then((data) => {
+        data.body.map((device) => {
+          trace('get-devices device: %0', device)
           if(knownDevices.hasOwnProperty(device.name)) {
             // We know about this device
             //debug("We already know about this device")
@@ -49,6 +53,8 @@ class ParticleManager extends Plugin {
               // it's state has changed
               debug("Device %s has gone %s", device.name,
                   device.connected ? "online" : "offline")
+              trace('Emitting: device-changed-state')
+              this.emit('device-changed-state', device.name)
             }
             // update the stored object with the new one
             knownDevices[device.name] = device
@@ -56,39 +62,35 @@ class ParticleManager extends Plugin {
             // we haven't seen this device before, add it to the list
             debug("New %s device: %s", device.connected ? "online" : "offline", device.name)
             knownDevices[device.name] = device
+            trace('new-device: %s : %s', device.name, device.connected)
+            this.emit('new-device', device.name, device.connected)
           }
-        }
-        this.emit('particle-devices-ready')
+        })
+      }).then(() => {
+        trace('Emitting: devices-ready')
+        this.emit('devices-ready')
       })
     } else {
       debug("Not logged in, check username and password")
+      return Promise.reject(new Error('Not logged in, check username and password'))
     }
   }
 
   callFunction(device, fn, args) {
-    return new Promise((resolve, reject) => {
-      if(accessToken != null) {
-        if(knownDevices.hasOwnProperty(device)) {
-          // The name is valid
-          var deviceid = knownDevices[device].id
-          var obj = {deviceId: deviceid, name: fn, argument: args, auth: accessToken}
-          particle.callFunction(obj).then(
-            (result) => {
-              debug('success: %o', result)
-              resolve(result)
-            },(err) => {
-              debug('error: %o', err)
-              reject(err)
-            })
-        } else {
-          debug("Unknown device: %s", device)
-          reject(`Unknown device: ${device}`)
-        }
+    if(accessToken != null) {
+      if(knownDevices.hasOwnProperty(device)) {
+        // The name is valid
+        var obj = {deviceId: knownDevices[device].id, name: fn, argument: args, auth: accessToken}
+        trace('callFunction: Calling: %s : Arguments: %s', fn.name, args)
+        return particle.callFunction(obj)
       } else {
-        debug("Not logged in, check username and password")
-        reject('Not logged in, check username and password')
+        debug("Unknown device: %s", device)
+        return Promise.reject(new Error(`Unknown device: ${device}`))
       }
-    })
+    } else {
+      debug("Not logged in, check username and password")
+      return Promise.reject(new Error('Not logged in, check username and password'))
+    }
   }
 
   getVariable(device, variable) {
@@ -96,6 +98,7 @@ class ParticleManager extends Plugin {
     if(accessToken != null) {
       if(knownDevices.hasOwnProperty(device)) {
         var obj = {deviceId: knownDevices[device].id, name: variable, auth: accessToken}
+        trace('getVariable: device: %s : variable %s', device, variable)
         return particle.getVariable(obj)
       } else {
         debug("Unknown device: %s", device)
@@ -106,41 +109,75 @@ class ParticleManager extends Plugin {
     }
   }
 
-  deviceStatus(device, callback) {
+  deviceStatus(device) {
     if(knownDevices.hasOwnProperty(device)) {
-      var returnVal = {[device]: knownDevices[device].status}
-      return callback == undefined ? returnVal : callback(null, returnVal)
+      trace('deviceStatus: device: %s : status: %s', device, knownDevices[device].connected)
+      return Promise.resolve(knownDevices[device].connected)
     } else {
-      return callback == undefined ? 'unknown device' : callback({error: 'unknown device'}, null)
+      debug('deviceStatus: rejecting %s as unknown device', device)
+      return Promise.reject(new Error(`Unknown device: ${device}`))
     }
   }
+  
+  /**
+   * At the moment, this is a useless function...I'll actually utilize it later
+  deviceRole(device) {
+    if(knownDevices.hasOwnProperty(device)) {
+      return this.deviceStatus().then((connected) => {
+        trace('deviceRole: Resolving %s : %s', device, connected)
+        return Promise.resolve(connected)
+      })
+    } else {
+      debug('deviceRole: Rejecting %s as unknown device', device)
+      return Promise.reject(new Error(`Unknown device: ${device}`))
+    }
+  }
+  */
 
   getDevicesByRole(role) {
-    debug('getDevicesByRole: %s', role)
+    debug('getDevicesByRole: Starting')
     if(accessToken != null) {
-      if(knownDevices.length == 0) {
+      trace('getDevicesByRole: AccessToken is good')
+      var numDevices = (Object.keys(knownDevices).length)
+      if(numDevices > 0) {
+        trace('getDevicesByRole: We have known devices: %s', numDevices) 
         var matches = []
-        for(var device in knownDevices) {
-          debug(device)
+        for(let device in knownDevices) {
+          trace('getDevicesByRole: inside the map: %s', device)
           if(knownDevices[device].connected) {
             debug('device %s is online, checking role', device)
             matches.push(new Promise((resolve, reject) => {
-              var d = device
               this.getVariable(device, 'role').then((result) => {
-                debug(d)
-                resolve(d)
+                trace('Result: %o', result)
+                debug('Matches? %s == %s: %s',result.body.result, role, result.body.result == role)
+                if(result.body.result == role) {
+                  trace('getDevicesByRole: resolving: %s', device)
+                  resolve(device)
+                } else {
+                  // it doesn't match, but we cant reject
+                  trace('getDevicesByRole: doesn\'t mach, resolve empty: %s', device)
+                  resolve()
+                }
               }).catch((err) => {
+                // resolve it as empty so it returns, we will filter later
+                trace('getDevicesByRole: resolve empty: %s', device)
                 resolve()
               })
             }))
           } 
         }
-        return Promise.all(matches)
+        return Promise.all(matches).then((matches) => {
+          return matches.filter((device) => {
+            trace('getDevicesByRole: filtering %s', device)
+            return device != undefined
+          })
+        })
       } else {
-        Promise.reject('No known devices, please wait until devices have been enumerated')
+        trace('getDevicesByRole: No known devices: %s', (Object.keys(knownDevices)).length)
+        return Promise.reject(new Error('No known devices, please wait until devices have been enumerated'))
       }
     } else {
-      return Promise.reject('Not logged in, check username and password')
+      return Promise.reject(new Error('Not logged in, check username and password'))
     }
   }
 }
